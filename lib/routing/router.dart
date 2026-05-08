5c6de019c8dbcedafbb9aaa5f11ca121a0d0c18d
LukGaming <thelukgaming333@gmail.com>
2026-05-08 09:18:15 -0300

    Badge de itens no carrinho

--- lib/routing/router.dart
+++ lib/routing/router.dart
@@ -3,6 +3,8 @@ import 'package:curso_mvvm_youtube/data/repositories/auth/auth_repository.dart';
 import 'package:curso_mvvm_youtube/routing/routes.dart';
 import 'package:curso_mvvm_youtube/ui/auth/login/view_models/login_viewmodel.dart';
 import 'package:curso_mvvm_youtube/ui/auth/login/widgets/login_screen.dart';
+import 'package:curso_mvvm_youtube/ui/cart/viewmodels/cart_viewmodel.dart';
+import 'package:curso_mvvm_youtube/ui/cart/widgets/cart_screen.dart';
 import 'package:curso_mvvm_youtube/ui/home/widgets/home_screen.dart';
 import 'package:curso_mvvm_youtube/ui/splash/widgets/splash_screen.dart';
 import 'package:flutter/material.dart';
@@ -20,11 +22,15 @@ GoRouter get router {
           loginViewmodel: LoginViewmodel(authRepository: getIt()),
         ),
       ),
-      GoRoute(path: Routes.home, builder: (context, state) => HomeScreen()),
+      GoRoute(
+        path: Routes.home,
+        builder: (context, state) => HomeScreen(cartViewModel: CartViewmodel()),
+      ),
       GoRoute(
         path: Routes.splash,
         builder: (context, state) => SplashScreen(authRepository: getIt()),
       ),
+      GoRoute(path: Routes.cart, builder: (context, state) => CartScreen()),
     ],
   );
 }
--- lib/ui/home/widgets/home_screen.dart
+++ lib/ui/home/widgets/home_screen.dart
@@ -1,19 +1,40 @@
 import 'package:curso_mvvm_youtube/config/dependencies.dart';
+import 'package:curso_mvvm_youtube/routing/routes.dart';
 import 'package:curso_mvvm_youtube/ui/cart/viewmodels/cart_viewmodel.dart';
 import 'package:curso_mvvm_youtube/ui/product/viewmodels/product_viewmodel.dart';
 import 'package:curso_mvvm_youtube/ui/product/widgets/products_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+  final CartViewmodel cartViewModel;
+  const HomeScreen({super.key, required this.cartViewModel});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Home Page")),
+      appBar: AppBar(
+        title: Text("Produtos"),
+        actions: [
+          ListenableBuilder(
+            listenable: cartViewModel,
+            builder: (context, child) {
+              return IconButton(
+                onPressed: () {
+                  context.push(Routes.cart);
+                },
+                icon: Badge.count(
+                  count: cartViewModel.items.length,
+                  child: Icon(Icons.shopping_cart),
+                ),
+              );
+            },
+          ),
+        ],
+      ),
       body: ProductsWidget(
         productViewmodel: ProductViewmodel(productRepository: getIt()),
-        cartViewmodel: CartViewmodel(),
+        cartViewmodel: cartViewModel,
       ),
     );
   }
